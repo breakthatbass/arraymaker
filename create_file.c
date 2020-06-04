80905baf9ed3f4e5ce9b91079ec5fce938dc5df3
breakthatbass <gamache.taylor@gmail.com>
2020-06-04 10:51:07 -0400

    clear .txt files

--- create_file.c
+++ create_file.c
@@ -1,3 +1,11 @@
+/*
+  Takes a file  and an int as an argument
+  the file arg is the name of the file to be created
+  the len arg is the length of the file -> i.e. the amount of lines of random numbers
+
+  it then generate random numbers and prints them to the file
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
