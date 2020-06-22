99da64612c8c9d6094006d6c2311dcbf5649909a
breakthatbass <gamache.taylor@gmail.com>
2020-06-22 13:45:18 -0400

    incorporate algorithm timing within program

--- src/main.c
+++ src/main.c
@@ -1,22 +1,20 @@
 /***
-
-  This file is the entry point to the program.
-
-  This program is meant to use alongside with algorithms as a way
-  to easily create and use large arrays to test timing
-  of algorithms.
-
-  It is meant to use alongside the UNIX time command.
-
-  It's a bit hard to get a sense of the timing of different sorting
-  and searching algorithms without very large arrays so it doesn't seem so
-  instantaneous.
-
-***/
+ * This is the entry point to the program
+ * 
+ * Arraymaker is used to create very large arrays of random numbers
+ * that are randomly sorted for the purpose of using them to learn
+ * about timing with sorting algorithms
+ * 
+ * The bigger the arrays to sort are, the more noticable
+ * the time differences can be.
+ * 
+ * Created by Taylor Gamache in June 2020
+ ***/
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h> 
 #include "arraymaker.h"
 
 int main(int argc, char *argv[])
@@ -53,42 +51,82 @@ int main(int argc, char *argv[])
 
 // SHELLSORT
       if (strcmp(algorithm, "shellsort") == 0) {
+        printf("Sorting...\n");
+         // start timing
+        clock_t t;
+        t = clock();
+        // call algorithm
         shellsort(outer_array, len);
-        //return 0;
+        t = clock() - t;
+        double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+        // end timing
+        printf("%s sorted %d elements in %f seconds\n", algorithm, len, time_taken);
+        return 0;
       }
+
 // BUBBLESORT
       else if (strcmp(algorithm, "bubblesort") == 0) {  
+        printf("Sorting...\n");
+        // start timing
+        clock_t t;
+        t = clock();
+        // call algorithm
         bubblesort(outer_array, len);
-        //return 0;
+        t = clock() - t;
+        double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+        // end timing
+        printf("%s sorted %d elements in %f seconds\n", algorithm, len, time_taken);
+        return 0;
       }
+
 // QUICKSORT
       else if (strcmp(algorithm, "quicksort") == 0) {
+        printf("Sorting...\n");
+        // start timing
+        clock_t t;
+        // call algorithm
+        t = clock();
         // qsort() comes from the C standard library and will
         // be better than any version I could write
         qsort(outer_array, len, sizeof(int), cmpfunc);
-        //return 0;
+        t = clock() - t;
+        double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+        // end timing
+        printf("%s sorted %d elements in %f seconds\n", algorithm, len, time_taken);
+        return 0;
       }
 // GNOMESORT      
       else if (strcmp(algorithm, "gnomesort") == 0) {  
+        printf("Sorting...\n");
+        // start timing
+        clock_t t;
+        t = clock();
+        // call algorithm
         gnome_sort(outer_array, len);
-        //return 0;
+        t = clock() - t;
+        double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+        // end timing
+        printf("%s sorted %d elements in %f seconds\n", algorithm, len, time_taken);
+        return 0;
       }
+
 // wrong sort
       else {
         printf("ERROR: %s is not available as an option. You can add your own to the files\n", algorithm);
         return 2;
       }
-
   }
+
   else {
     printf("Usage: arraymaker\n");
     printf("\t\tcreate num-of-elements file.txt\n");
     printf("\t\tsort file.txt algorithm\n");
     return 3;
   }
+  /* no printing for now
   for (i = 0; i < len; ++i) {
     printf("%d: %d\n", i+1, *(outer_array + i));
   } 
-  
+  */
   return 0;
 }
