b2535a6ec71f3491649ef05c73115462dd3067b4
breakthatbass <gamache.taylor@gmail.com>
2020-06-29 16:28:50 -0400

    add insertion sort algorithm

---
+++ algorithms/insertionsort.c
@@ -0,0 +1,16 @@
+
+void insertion_sort(int array[], int n)
+{
+  int i, key_item, j;
+  for (i = 0; i < n; i++) {
+    key_item = array[i];
+
+    j = i - 1;
+
+    while (j >= 0 && array[j] > key_item) {
+      array[j+1] = array[j];
+      j--;
+    }
+    array[j + 1] = key_item;
+  }
+}
